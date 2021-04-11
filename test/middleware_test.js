066576718950a3b6443f2e02186aa17a5c5a2a46
Rohan <mrohan171093@gmail.com>
2021-04-11 18:03:57 +0530

    middleware

---
+++ test/middleware_test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const User = require('../src/user');
+const BlogPost = require('../src/blogPost');
+
+describe('MiddleWare', () => {
+  let joe, blogPost;
+  beforeEach((done) => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({ title: 'JS is great', content: 'This is awesome' });
+
+    joe.blogPosts.push(blogPost);
+
+    Promise.all([joe.save(), blogPost.save()]).then(() => done());
+  });
+
+  it('users clean up dangling blogposts on remove', (done) => {
+    joe
+      .remove()
+      .then(() => BlogPost.count())
+      .then((blogCnt) => {
+        console.log(blogCnt);
+        assert(blogCnt === 0);
+        done();
+      });
+  });
+});
